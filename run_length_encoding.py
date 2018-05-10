a969dd6218f7e5e5f4fb87d783d1f20527da4153
Kev Kipkemoi <kevkipkemoi@outlook.com>
2018-05-10 23:47:44 +0300

    case study: run length encoding, executor

--- run_length_encoding.py
+++ run_length_encoding.py
@@ -19,3 +19,26 @@ def compress_chunk(chunk):
         count += 1
     compressed.append(count | (128 * last))
     return compressed
+'''This function accepts a bitarray named row. It splits into
+chunks that are 127 bits wide
+'''
+def compress_row(row):
+    compressed = bytearray()
+    chunks = split_bits(row, 127)
+    for chunk in chunks:
+        compressed.extend(compress_chunk(chunk))
+    return compressed
+
+'''Wrap these functions in a method that runs everything
+in an executor, since we are noy certain whether it will
+run effectively in a thread or process
+'''
+def compress_in_executor(executor, bits, width):
+    row_compressors = []
+    for row in split_bits(bits, width):
+        compressor = executor.submit(compress_row, row)
+        row_compressors.append(compressor)
+
+    compressed = bytearray()
+    for compressor in row_compressors:
+        compressed.extend(compressor.result())
